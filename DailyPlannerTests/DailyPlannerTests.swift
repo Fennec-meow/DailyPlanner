42a785b9f9b1b4c1e89703d15f444c943d625cf6
Fennec-meow <krisbojchenko@gail.com>
2024-12-08 14:51:06 +0300

    Initial Commit

---
+++ DailyPlannerTests/DailyPlannerTests.swift
@@ -0,0 +1,17 @@
+//
+//  DailyPlannerTests.swift
+//  DailyPlannerTests
+//
+//  Created by Kira on 08.12.2024.
+//
+
+import Testing
+@testable import DailyPlanner
+
+struct DailyPlannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
